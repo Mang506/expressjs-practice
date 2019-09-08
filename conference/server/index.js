85c01206019510cb61277092e53da3930dfacd87
Mang506 <mrubly@live.com>
2019-09-08 08:07:36 -0700

    template variables and dynamic data

--- conference/server/index.js
+++ conference/server/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const createError = require('http-errors');
 const path = require('path');
 const configs = require('./config');
+const SpeakerServices = require('./services/SpeakerServices');
 const app = express();
 
 const config = configs[app.get('env')];
+
+const speakerService = new SpeakerServices(config.data.speakers);
 
 app.set('view engine', 'pug'); // set pug as template engine
 if(app.get('env') === 'development'){
@@ -13,12 +16,22 @@ if(app.get('env') === 'development'){
 app.set('views', path.join(__dirname, './views')); //default: views folder in application root directory
 app.locals.title = config.sitename; //global variable
 
-
 const routes = require('./routes'); // loads routes module
 app.use(express.static('public')); // serves static files
 app.get('/favicon.ico', (req, res, next) => {
   return res.sendStatus(204); // No favicon, send no content handler
 });
+
+app.use(async (req, res, next) => {
+  try {
+    const names = await speakerService.getNames();
+    console.log(names);
+    res.locals.speakerNames = names;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+})
 
 app.use('/', routes()); // loads index from routes folder
 
